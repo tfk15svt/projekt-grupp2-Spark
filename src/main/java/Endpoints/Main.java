f0366f11b4ae7e118af41a8c789e7040cef4d4b6
Veiret <Veiret@130.243.2.79>
2018-04-09 11:28:50 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/Broker/SeasonBroker.java
    	src/main/java/com/mycompany/sportstatsveiret/Game.java
    	src/main/java/com/mycompany/sportstatsveiret/Round.java
    	src/main/java/com/mycompany/sportstatsveiret/Season.java

--- src/main/java/Endpoints/Main.java
+++ src/main/java/Endpoints/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sportstatsveiretspark;
+package Endpoints;
 
 import static spark.Spark.get;
 
